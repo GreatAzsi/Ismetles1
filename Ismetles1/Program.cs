6401909ea1ffe2ef37bee71ec38fc067024e6934
GreatAzsi <akospotfiok@gmail.com>
2026-09-07 22:02:04 +0200

    Did the First Task

--- Ismetles1/Program.cs
+++ Ismetles1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Bevezetés
+
+            Console.WriteLine("Adj meg tetszőleges mennyiségű egész számokat egyetlen sorban, vesszővel elválasztva.");
+            List<int> szamok = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            foreach (int i in szamok)
+            {
+                Console.WriteLine(i);
+            }
+
+            // 1. feladat
+            // Adja vissza a listában lévő legnagyobb számot
+            int max = szamok.Max();
+            Console.WriteLine("A legnagyobb szám: " + max);
+
         }
     }
 }
