450c87d812c74f3006da6c995cb0d8ec858ffa7b
GreatAzsi <akospotfiok@gmail.com>
2026-09-07 22:04:08 +0200

    Did the Second Task

--- Ismetles1/Program.cs
+++ Ismetles1/Program.cs
@@ -1,23 +1,35 @@
-﻿namespace Ismetles1
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Ismetles1;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Bevezetés
+
+        Console.WriteLine("Adj meg tetszőleges mennyiségű egész számokat egyetlen sorban, vesszővel elválasztva.");
+        List<int> szamok = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+        foreach (int i in szamok)
         {
-            // Bevezetés
-
-            Console.WriteLine("Adj meg tetszőleges mennyiségű egész számokat egyetlen sorban, vesszővel elválasztva.");
-            List<int> szamok = Console.ReadLine().Split(',').Select(int.Parse).ToList();
-            foreach (int i in szamok)
-            {
-                Console.WriteLine(i);
-            }
-
-            // 1. feladat
-            // Adja vissza a listában lévő legnagyobb számot
-            int max = szamok.Max();
-            Console.WriteLine("A legnagyobb szám: " + max);
-
+            Console.WriteLine(i);
         }
+
+        // 1. feladat
+        // Adja vissza a listában lévő legnagyobb számot
+        int max = szamok.Max();
+        Console.WriteLine("A legnagyobb szám: " + max);
+
+        // 2. feladat
+        // Számítsa ki és adja vissza a listában található számok átlagát.
+        // Figyelj arra, hogy az eredmény lebegőpontos szám legyen.
+        int sum = 0;
+        foreach (int i in szamok)
+        {
+            sum += i;
+        }
+        double atlag = (double)sum / szamok.Count;
+        Console.WriteLine("A számok átlaga: " + atlag);
+
     }
 }
