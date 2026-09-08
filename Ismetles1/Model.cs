513ac0edef2f9fea174f256de83ea85d9db6f838
Bocsa.akos <Bocsa.akos@C19-08>
2026-09-08 09:27:18 +0200

    Fixed FileManager

--- Ismetles1/Model.cs
+++ Ismetles1/Model.cs
@@ -35,5 +35,38 @@ namespace Ismetles1
             TypeName = typeName;
         }
     }
+    public class FileManager
+    {
+        public static (List<Plane>, List<PlaneType>) Readfile(string path)
+        {
+            try
+            {
+                List<Plane> All = new();
+                List<PlaneType> All2 = new();
+                List<PlaneType> All2Distinct = new();
+
+                foreach (string line in File.ReadAllLines(path).Skip(1))
+                {
+
+                    string[] temp = line.Split(";");
+
+                    if (temp.Length == 7)
+                    {
+                        All.Add(new Plane(int.Parse(temp[0]), temp[1], int.Parse(temp[2]), int.Parse(temp[3]), int.Parse(temp[4]), int.Parse(temp[5])));
+
+                        All2.Add(new PlaneType(int.Parse(temp[5]), temp[6]));
+                    }
+                }
+                All2Distinct = All2.GroupBy(PlaneType => PlaneType.TypeId).Select(group => group.First()).ToList();
+                return (All, All2Distinct);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
 
 }
+
