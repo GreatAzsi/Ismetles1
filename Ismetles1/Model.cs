4103508d5f0404ec602c6ebdf2b0601e3fb615b8
Bocsa.akos <Bocsa.akos@C19-08>
2026-09-08 09:18:24 +0200

    Made the FileManager Function. (I think?)

--- Ismetles1/Model.cs
+++ Ismetles1/Model.cs
@@ -35,5 +35,37 @@ namespace Ismetles1
             TypeName = typeName;
         }
     }
+    public class FileManager
+    {
+        public static (List<Plane> ,List<PlaneType>) Readfile(string path)
+        {
+            try
+            {
+                List<Plane> All = new();
+                List<PlaneType> All2 = new();
 
+                foreach (string line in File.ReadAllLines(path).Skip(1))
+                {
+
+                    string[] temp = line.Split(";");
+
+                    if (temp.Length == 7)
+                    {
+                        All.Add(new Plane(int.Parse(temp[0]), temp[1], int.Parse(temp[2]), int.Parse(temp[3]), int.Parse(temp[4]), int.Parse(temp[5]), int.Parse(temp[6])));
+
+                        All2.Add(new PlaneType(int.Parse(temp[6]), temp[7]));
+
+                        List<PlaneType> All2Distinct = All2.GroupBy(PlaneType => PlaneType.TypeId).Select(group => group.first()).ToList();
+                    }
+                }
+                return (All,All2Distinct);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+    
 }
